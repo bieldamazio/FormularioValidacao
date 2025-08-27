dfd31fda43d8d83cc57176b7d0d840a702c2c759
bieldamazio <gabriel.damazio@aluno.senai.br>
2025-08-27 15:40:33 -0300

    adicionando verificacao

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ iconSenha.forEach(icon => {
         icon.classList.toggle('fa-eye');
     })
 });
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const iconError = '<i class="fa-solid fa-circle-exclamation"></i>';
+    const nome = document.querySelector('#nome');
+    const nomeValue = nome.value;
+    const inputBox = nome.closest(".input_box");
+    const errorSpan = inputBox.querySelector(".erro");
+    errorSpan.innerHTML = '';
+    inputBox.classList.remove('invalid');
+    inputBox.classList.add('valid');
+
+    if(isEmpty(nomeValue)){
+        errorSpan.innerHTML = `${iconError} O campo é obrigatório!`;
+        inputBox.classList.remove('valid');
+        inputBox.classList.add('invalid');
+        return;
+    }
+});
+
+function isEmpty(value) {
+    return value === '';
+}
